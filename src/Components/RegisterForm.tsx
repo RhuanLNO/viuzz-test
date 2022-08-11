d983706e2bdea8d729323f9f8fe93cf6e2ffacfe
Rhuan Levy <rhuan_os@hotmail.com>
2022-08-10 22:42:35 -0300

    Adicionado condicional para formulário de edição

--- src/Components/RegisterForm.tsx
+++ src/Components/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import { Typography, Button, Grid } from "@mui/material";
+import { Typography, Grid } from "@mui/material";
 import { useEffect, useState } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -6,36 +6,32 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import axios from "axios";
 import { CenterGrid, JobText, TownForm, SendButton, SucAlert } from '../assets/Styled';
 import { useForm } from "react-hook-form";
-import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
 
-const Register = () => {
+const Register = (props: {toggleShowEmployees: React.MouseEventHandler<HTMLButtonElement> | any | undefined}) => {
 
   const [data, setData] = useState<any>([]);
   const [town, setTown] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [ saveIndex, setSaveIndex ] = useState<any>(null);
 
   type FormData = {
     name: string;
     job: string;
   };
 
-  const { register, getValues, handleSubmit, formState:{errors} } = useForm<FormData>();
+  const { register, getValues, setValue, formState:{errors} } = useForm<FormData>();
 
   const onSubmit=(e: any) => {
     e.preventDefault();
     setIsSubmitted(true);
-
   };
 
   const FetchData= () =>{ axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/distritos')
   .then((response) => {
-    const newData = response
-    const Datinha = response.data
-    /* setData(newData.data); */
-    const nomesDatinha = Datinha.map((data: { nome: any; municipio: any; }) => data.municipio.nome)
-    const unique = [...new Set(nomesDatinha)]
-    console.log(unique)
+    const newData = response.data
+    const nomesData = newData.map((data: { nome: any; municipio: any; }) => data.municipio.nome)
+    const unique = [...new Set(nomesData)]
     setData(unique);
     return unique
     })
@@ -44,14 +40,21 @@ const Register = () => {
 
   useEffect(() => {
     FetchData();
+    const testIndex = localStorage.getItem("index");
+    setSaveIndex(testIndex);
+    if (testIndex){
+      const employees = JSON.parse(localStorage.getItem("employees")!);
+      const selectEmployee = employees[testIndex];
+      console.log(selectEmployee);
+      setValue("name", selectEmployee.name);
+      setValue("job", selectEmployee.job);
+      setTown(selectEmployee.town);
+      console.log(getValues("name"));
+    }
   }, []);
 
   const handleChange = (event: SelectChangeEvent) => {
     setTown(event.target.value as string);
-    /* localStorage.setItem("Cidade", event.target.value); */
-    let cityhistory = JSON.parse(localStorage.getItem("city")!) || [];
-    cityhistory.push(event.target.value);
-    localStorage.setItem("city", JSON.stringify(cityhistory));
   };
 
   const saveInfor = (e: any) => {
@@ -61,29 +64,25 @@ const Register = () => {
       town: town
     };
     let listEmployees = JSON.parse(localStorage.getItem("employees")!) || [];
-    listEmployees.push(newEmployee);
+    if (saveIndex) {
+      listEmployees.splice(saveIndex, 1, newEmployee);
+      props.toggleShowEmployees();
+    } else {
+      listEmployees.push(newEmployee);
+    }
     localStorage.setItem("employees", JSON.stringify(listEmployees));
-    //console.log(listEmployees);
-    // e.preventDefault();
-    // const names = getValues("name")
-    // const jobs = getValues("job")
-    // let namehistory = JSON.parse(localStorage.getItem("name")!) || [];
-    // namehistory.push(names);
-    // localStorage.setItem("name", JSON.stringify(namehistory));
-    // let jobhistory = JSON.parse(localStorage.getItem("job")!) || [];
-    // jobhistory.push(jobs);
-    // localStorage.setItem("job", JSON.stringify(jobhistory));
-  }
-  const values = getValues("name")
-  const values2 = getValues("job")
-  console.log(values, values2)
+    setValue("name", "");
+    setValue("job", "");
+    setTown("");
+    localStorage.removeItem("index");
+  };
 
   return (
     <Grid container justifyContent="center">
       <CenterGrid container item md={3} flexDirection="column" >
         <Grid container width="100%">
           <form onSubmit={onSubmit}>
-            <Typography align="center">Cadastrar novo funcionário</Typography>
+            {saveIndex ? <Typography align="center">Editar Funcionário</Typography> :<Typography align="center">Cadastrar novo funcionário</Typography>}
             <JobText id="name" label="Nome" variant="outlined"  
             {...register("name", {
               required: "Required"
@@ -112,7 +111,7 @@ const Register = () => {
                   )}
                 </Select>
             </TownForm>
-            <SendButton variant="outlined" value="Register" type="submit" onClick={saveInfor}>Enviar</SendButton>
+            <SendButton variant="outlined" value="Register" type="submit" onClick={saveInfor}>Salvar</SendButton>
           </form>
           {isSubmitted ? <SucAlert severity="success">
           <AlertTitle><strong>Sucesso</strong></AlertTitle>
